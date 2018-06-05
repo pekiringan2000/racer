a9c7e9d4edc4c750aa0fcc62264662a08e3c84f3
pekiringan2000 <38562425+pekiringan2000@users.noreply.github.com>
2018-06-05 21:28:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ var players = []
 var panjangLintasan;
 var maxPlayer;
 var currentPlayer; 
+var isWin=false ;
 
 function generateRace () {
     //console.log('loloolololo')
     panjangLintasan = document.getElementById('input-kolom').value;
     currentPlayer = 0;
-    maxLintasan = panjangLintasan;
+
     maxPlayer = document.getElementById('input-baris').value;
 
+   if (panjangLintasan>10){     
     for (i=0; i < maxPlayer; i++) {
         player = {
             name: cadanganPlayer[i],
@@ -23,9 +25,13 @@ function generateRace () {
         players.push(player)
     }
     printRace()
+    } else { console.log ('tidak dapat lanjut')}
 }
 
 function rollDice () {
+    if (isWin) {
+       return
+    }
     var randomNumber = Math.floor(Math.random() *6) + 1;
     if (currentPlayer === Number(maxPlayer)) {
         currentPlayer = 0
@@ -35,7 +41,9 @@ function rollDice () {
         currentPlayer++
     } 
     printRace()
+
 }
+
 
 function printRace () {
     containerGame.innerHTML='';
@@ -49,13 +57,25 @@ function printRace () {
             var divKolom = document.createElement('div');
                     divKolom.id = 'box'+i+j;
                     divKolom.className='box';
-// untuk membuat posisi player                    
-            if (j === players[i].position) {
-                divKolom.style.backgroundImage = players[i].name;
+        // untuk membuat posisi player                    
+            if (j === players[i].position) {    
+                divKolom.style.backgroundImage = "url(" +players[i].name + ")";
+            //untuk menambah var ke string "+"
+            }
+            if (players[i].position >= Number(panjangLintasan)) {
+                isWin === true
+                divKolom.style.backgroundImage = "url(" +players[i].name + ")";
+                winner()
             }
 
             divBaris.appendChild(divKolom)
         }
         containerGame.appendChild(divBaris)
+    }
+}
+
+function winner() {
+    if (isWin) {
+        alert('WIN');
     }
 }
