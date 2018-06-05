3deeb2366841f79794eb5f28f4be9ed74629e2b2
pekiringan2000 <38562425+pekiringan2000@users.noreply.github.com>
2018-06-05 18:56:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,131 @@
+// var containerGame = document.getElementById('containerGame');
+
+// var player = [{
+//     nameOfPlayer: "A",
+//     position:0
+// },{
+//     nameOfPlayer: "B",
+//     position:0
+// },{
+//     nameOfPlayer: "C",
+//     position:0
+// }]
+
+// var currentPlayer = player[0]; 
+
+// function generateBoard(){
+// //genearate board for game
+// //clear existing board
+
+// containerGame.innerHTML=''
+
+// var jumlahBaris = document.getElementById('input-baris').value
+// var jumlahKolom = document.getElementById('input-kolom').value
+
+// for (i=0; i<jumlahBaris;i++){
+//      var divBaris = document.createElement('div')
+//      divBaris.id = 'baris-'+i
+//      divBaris.className = 'containerBaris'
+
+//     for (j=0; j<jumlahKolom; j++) {
+//         var divKolom = document.createElement('div')
+//         divKolom.id = 'box'+i+j
+//         divKolom.className='box'
+
+//         divBaris.appendChild(divKolom)
+//     }
+//     containerGame.appendChild(divBaris)
+//  }
+// console.log(containerGame)   
+// }
+
+// //roll dice
+
+//  function roll() {
+//     var randomNumber = Math.floor(Math.random() *6) + 1;
+//     return randomNumber;
+//   }
+
+//   //Prints dice roll to the page
+
+// function printNumber(number) {
+//     var placeholder = document.getElementById('placeholder');
+//     placeholder.innerHTML = number;
+//   }
+  
+// var rolldicebutton = document.getElementById('rolldicebutton');
+
+
+
+// function generatedice() {
+//     var result = roll();
+//     printNumber(result);
+// //    player[0].position = player[0].position+result;
+//     currentPlayer.position=currentPlayer.position+result;
+//     console.log("===>",player);
+//     move();
+//   }
+
+// function move() {
+//     document.getElementById('box0'+currentPlayer.position).style.backgroundColor = "red";
+// }      
+
+
 var containerGame = document.getElementById('containerGame');
+var players = [
+    {
+        name: 0,
+        position: 0
+    }]
 
-var player = [{
-    nameOfPlayer: "A",
-    position:0
-},{
-    nameOfPlayer: "B",
-    position:0
-}]
+var panjangLintasan;
+var maxPlayer;
+var currentPlayer 
 
-function generateBoard(){
-//genearate board for game
-//clear existing board
+function generateRace () {
+    console.log('loloolololo')
+    panjangLintasan = document.getElementById('input-kolom').value;
+    currentPlayer = 0;
+    maxLintasan = panjangLintasan;
+    maxPlayer = document.getElementById('input-baris').value;
 
-containerGame.innerHTML=''
-
-var jumlahBaris = document.getElementById('input-baris').value
-var jumlahKolom = document.getElementById('input-kolom').value
-
-for (i=0; i<jumlahBaris;i++){
-     var divBaris = document.createElement('div')
-     divBaris.id = 'baris-'+i
-     divBaris.className = 'containerBaris'
-
-    for (j=0; j<jumlahKolom; j++) {
-        var divKolom = document.createElement('div')
-        divKolom.id = 'box'+i+j
-        divKolom.className='box'
-
-        divBaris.appendChild(divKolom)
+    for (i=0; i<maxPlayer-1; i++) {
+        player = {
+            name:i ,
+            position:0
+        }
+        players.push(player)
     }
-    containerGame.appendChild(divBaris)
- }
-console.log(containerGame)   
 }
 
-//roll dice
 
- function roll() {
+function rollDice () {
     var randomNumber = Math.floor(Math.random() *6) + 1;
-    return randomNumber;
-  }
+    players[currentPlayer].position += randomNumber;
 
-  //Prints dice roll to the page
+    if (currentPlayer<maxPlayer) {
+        currentPlayer++
+     }
+}
 
-function printNumber(number) {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = number;
-  }
-  
-var rolldicebutton = document.getElementById('rolldicebutton');
-  
-function generatedice() {
-    var result = roll();
-    printNumber(result);
-    player[0].position = player[0].position+result;
-    console.log("===>",player);
-    move();
-  }
-  
-function move() {
-    document.getElementById('box0'+player[0].position).style.backgroundColor = "red";
-}      
+function printRace () {
+    containerGame.innerHTML='';
+    for (i=0; i<maxPlayer; i++){
+        var divBaris = document.createElement('div')
+            divBaris.id = 'baris-'+i
+            divBaris.className = 'containerBaris'
+
+        for (j=0;j<panjangLintasan;j++) {
+            var divKolom = document.createElement('div');
+                    divKolom.id = 'box'+i+j;
+                    divKolom.className='box';
+
+            if (j === position) {
+                players[currentPlayer].position.style.backgroundColor = "red";
+            }
+
+            divBaris.appendChild(divKolom)
+        }
+        containerGame.appendChild(divBaris)
+    }
+}
